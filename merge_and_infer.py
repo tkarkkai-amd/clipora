6d1e39b4297307a3ddc848a8e00473b489058293
Teemu Kärkkäinen <tkarkkai@amd.com>
2025-08-12 08:30:10 +0300

    inference fix

--- merge_and_infer.py
+++ merge_and_infer.py
@@ -104,7 +104,7 @@ def run_single_inference(job_id, image_path, classes: list[str]):
     lora_adapter_path = job_dict['best_finetuned_model_path']
     # If relative path, assume it's relative to TRAIN_JOB_OUTPUT_DIR
     if not os.path.isabs(lora_adapter_path):
-        lora_adapter_path = os.path.join(TRAIN_JOB_OUTPUT_DIR, job_id, lora_adapter_path)
+        lora_adapter_path = os.path.join(TRAIN_JOB_OUTPUT_DIR, lora_adapter_path)
     config_path = os.path.join(lora_adapter_path, "clipora_config.yaml")
     config = parse_yaml_to_config(config_path)
     lora_model, preprocess = init_model(config, lora_adapter_path=lora_adapter_path)
